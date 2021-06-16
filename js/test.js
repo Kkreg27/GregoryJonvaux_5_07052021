ecf2758917283074f3839dc7e4096ee06bb587c0
Grégory Jonvaux <jonvauxgregory@gmail.com>
2021-06-16 16:37:31 +0200

    rectification multiproduit panier

--- js/test.js
+++ js/test.js
@@ -99,24 +99,34 @@ function recupererProduit(infoProduit) {
   //
   //
   //
-  localStorage.setItem("panier", JSON.stringify([]));
+  if (!localStorage.getItem("panier")) {
+    localStorage.setItem("panier", JSON.stringify([]));
+  }
   //
   buttonAjouterPanier.addEventListener("click", function () {
     let selectElmt = document.querySelector(".selectLentilles");
     let lentillesCustom = selectElmt.options[selectElmt.selectedIndex].value;
 
-    let id = infoProduit.name;
+    let id = infoProduit._id;
+    let name = infoProduit.name;
     let lense = lentillesCustom;
-    let elem = {
+    let price = infoProduit.price;
+    let imageUrl = infoProduit.imageUrl;
+
+    let panierBrut = localStorage.getItem("panier");
+
+    let panier = JSON.parse(panierBrut);
+
+    let newElem = {
       id,
+      name,
       lense,
+      price,
+      imageUrl,
       qty: 1,
     };
 
-    let panierBrut = localStorage.getItem("panier");
-    console.log(panierBrut);
-    let panier = JSON.parse(panierBrut);
-    panier.push(elem);
+    panier.push(newElem);
     localStorage.setItem("panier", JSON.stringify(panier));
   });
 
@@ -145,8 +155,87 @@ function creePanier() {
 
   let panierBrut = localStorage.getItem("panier");
   let panier = JSON.parse(panierBrut);
-  console.log(panierBrut);
-  divPanier.innerText = panier;
+  console.log(panier);
+
+  let infoProduitImagePanier = document.createElement("div");
+  infoProduitImagePanier.classList.add("infoProduitImagePanier");
+  divPanier.appendChild(infoProduitImagePanier);
+
+  let infoImgProduitImagePanier = document.createElement("p");
+  infoImgProduitImagePanier.classList.add("infoImgProduitImagePanier");
+
+  infoProduitImagePanier.appendChild(infoImgProduitImagePanier);
+
+  let infoNomProduitImagePanier = document.createElement("p");
+  infoNomProduitImagePanier.classList.add("infoNomProduitImagePanier");
+  infoNomProduitImagePanier.innerText = "Appareil";
+  infoProduitImagePanier.appendChild(infoNomProduitImagePanier);
+
+  let infoLentilleProduitImagePanier = document.createElement("p");
+  infoLentilleProduitImagePanier.classList.add(
+    "infoLentilleProduitImagePanier"
+  );
+  infoLentilleProduitImagePanier.innerText = "Lentilles";
+  infoProduitImagePanier.appendChild(infoLentilleProduitImagePanier);
+
+  let infoQuantiteProduitImagePanier = document.createElement("p");
+  infoQuantiteProduitImagePanier.classList.add(
+    "infoQuantiteProduitImagePanier"
+  );
+  infoQuantiteProduitImagePanier.innerText = "Quantité";
+  infoProduitImagePanier.appendChild(infoQuantiteProduitImagePanier);
+
+  let infoPrixProduitImagePanier = document.createElement("p");
+  infoPrixProduitImagePanier.classList.add("infoPrixProduitImagePanier");
+  infoPrixProduitImagePanier.innerText = "Prix";
+  infoProduitImagePanier.appendChild(infoPrixProduitImagePanier);
+
+  let infoTrashProduitImagePanier = document.createElement("p");
+  infoTrashProduitImagePanier.classList.add("infoTrashProduitImagePanier");
+
+  infoProduitImagePanier.appendChild(infoTrashProduitImagePanier);
+
+  for (let elem of panier) {
+    let blocProduitPanier = document.createElement("div");
+    blocProduitPanier.classList.add("blocProduitPanier");
+    divPanier.appendChild(blocProduitPanier);
+
+    let photoProduitPanier = document.createElement("img");
+    photoProduitPanier.classList.add("photoProduitPanier");
+    let src = photoProduitPanier.getAttribute("src");
+    photoProduitPanier.src = elem.imageUrl;
+    blocProduitPanier.appendChild(photoProduitPanier);
+
+    let nomProduitPanier = document.createElement("p");
+    nomProduitPanier.classList.add("nomProduitPanier");
+    nomProduitPanier.innerText = elem.name;
+    blocProduitPanier.appendChild(nomProduitPanier);
+
+    let optionProduitPanier = document.createElement("p");
+    optionProduitPanier.classList.add("optionProduitPanier");
+    optionProduitPanier.innerText = elem.lense;
+    blocProduitPanier.appendChild(optionProduitPanier);
+
+    let quantiteProduitPanier = document.createElement("p");
+    quantiteProduitPanier.classList.add("quantiteProduitPanier");
+    quantiteProduitPanier.innerText = elem.qty;
+    blocProduitPanier.appendChild(quantiteProduitPanier);
+
+    let prixProduitPanier = document.createElement("p");
+    prixProduitPanier.classList.add("prixProduitPanier");
+    prixProduitPanier.innerText = `${elem.price / 100} €`;
+    blocProduitPanier.appendChild(prixProduitPanier);
+
+    let corbeilleProduitPanier = document.createElement("button");
+    corbeilleProduitPanier.classList.add("corbeilleProduitPanier");
+    corbeilleProduitPanier.innerHTML = `<i class="far fa-trash-alt"></i>`;
+    blocProduitPanier.appendChild(corbeilleProduitPanier);
+  }
+
+  let totalProduit = document.createElement("p");
+  totalProduit.classList.add("totalProduit");
+  totalProduit.innerText = "total";
+  divTotal.appendChild(totalProduit);
 }
 
 creePanier();
